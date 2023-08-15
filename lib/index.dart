ef3e42ee639c6122096ade18355c71fd9258c882
bastndev <gohit24@gmail.com>
2023-08-15 11:38:00 -0500

    :cherries: Update `Basic Types`

--- lib/index.dart
+++ lib/index.dart
@@ -15,8 +15,18 @@
  */
 
 //--- -- - -- - FIXME: Print 
-void main(){
+/* void main(){
   String myName = 'Gohit';
   String lastName = 'Bastian';
   print('Hello 👋\nMy name is: $myName and my last name is: $lastName');
+} */
+
+//--- -- - -- - FIXME:  Basic Types
+void main(){
+  String name = 'Gohit';
+  int age = 25;
+  double height = 1.70;
+  bool dota2 = true;
+
+  print('${name} \n${age} \n${height} \n${dota2}');
 }
