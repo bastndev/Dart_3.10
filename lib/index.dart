839adaabd1ee02e2cefbd3ad154e956e0adca084
bastndev <gohit24@gmail.com>
2023-08-15 10:28:28 -0500

    :cherries: Update `Print`

--- lib/index.dart
+++ lib/index.dart
@@ -5,4 +5,17 @@
 }
  */
 
-// --- - -- -- TODO: 
+// --- - -- -- TODO:  Hello < 3;
+/* void main (){
+
+  for (int i = 0; i <= 3; i++){
+    print("Hello ${i +1 }");
+  }
+}
+ */
+
+//--- -- - -- - FIXME: Print 
+void main(){
+  String name = 'Andre';
+  print('Name: $name');
+}
