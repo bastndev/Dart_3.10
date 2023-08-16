5d2669752df7f00e2c53a912b9e5565ae68d2d61
bastndev <gohit24@gmail.com>
2023-08-16 11:12:11 -0500

    :tomato: Update `Printing`

--- lib/index.dart
+++ lib/index.dart
@@ -22,11 +22,21 @@
 } */
 
 //--- -- - -- - FIXME:  Basic Types
-void main(){
+/* void main(){
   String name = 'Gohit';
   int age = 25;
   double height = 1.70;
   bool dota2 = true;
 
   print('${name} \n${age} \n${height} \n${dota2}');
+} */
+
+//--- --- --- --- TODO:
+void main(){
+  String firstName = 'Andrea';
+  String lastName = 'Bizzotto';
+  int age = 36;
+  double height = 1.84;
+
+  print("Hello 👋 \nMy name is: ${firstName} ${lastName} \nand I have: ${age} years old \nI height: ${height}m");
 }
