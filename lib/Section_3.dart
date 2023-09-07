436bf43985c3f811af8e487aec8c725708d7644e
bastndev <gohit24@gmail.com>
2023-09-07 09:46:40 -0500

    :avocado: Update `new`

--- lib/Section_3.dart
+++ lib/Section_3.dart
@@ -8,9 +8,36 @@
 
 //--- TODO: 28. Static vs Dynamic Languages
 
-void main () {
+/* void main () {
   String name = 'Gohit';
 
   print(name.toUpperCase());
   print(name.toLowerCase());
-}
+} */
+
+//---TODO: 29. Type inference with var
+/* void main (){
+  var name = 'Gohit';
+  var age = 36;
+  age = true;
+  var height = 1.70;
+} */
+
+//--- TODO: 30. The final keyword
+
+/* void main () {
+  final name = 'Gohit';
+  final age = 25;
+  final height = 1.70;
+
+} */
+//FIXME: No nesesary add (INT - String and - double) 
+//Es estricto est a Loca
+/* void main (){
+  final title = 'Dart Course';
+  final titleUppercase = title.toUpperCase();
+
+  print(titleUppercase);
+} */
+
+// --- TODO: 31. The const keyword
