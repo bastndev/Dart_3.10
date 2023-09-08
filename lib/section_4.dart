bd35b640319ab5046f84cf584b821ee3ea927c2e
bastndev <gohit24@gmail.com>
2023-09-08 09:12:21 -0500

    :pomegranate:

--- lib/section_4.dart
+++ lib/section_4.dart
@@ -1,1 +1,16 @@
 // ---TODO: Control flow
+//- FIXME: 35. If/else statements
+void main() {
+  const age = 40;
+  if (age < 17) {
+    print("Welcome Junior");
+    print("Price is \$10");
+  } else if (age >= 60) {
+    print("Senior ticket");
+    print("price is \$6");
+  } else {
+    print("Regular Ticket");
+    print("price is \$10");
+  }
+  print("Enjoy your visit");
+}
