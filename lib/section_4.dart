48c740c66e65a0ce1503547d26b3e49acb2f70c4
bastndev <gohit24@gmail.com>
2023-09-12 11:35:26 -0500

    :coconut: Update `new`

--- lib/section_4.dart
+++ lib/section_4.dart
@@ -311,8 +311,54 @@ void main() {
 }
  */
 
+//FIXME: Preview
+/* enum Operation { plus, minus, multiply, divide }
+
+void main() {
+  const a = 4;
+  const b = 2;
+  const op = Operation.plus;
+
+  switch (op) { 
+    case Operation.plus:
+      print(a + b);
+      break;
+    case Operation.minus:
+      print(a - b);
+      break;
+    case Operation.multiply:
+      print(a * b);
+      break;
+    case Operation.divide:
+      print(a / b);
+      break;
+    default:
+      print("Operación no válida");
+  }
+} */
 
 // --- TODO: ---  Practice --- TODO: //
-void main() {
+enum Operation { plus, minus, multiply, divide }
 
+void main() {
+  const a = 4;
+  const b = 2;
+  const op = Operation.minus;
+
+  switch (op) {
+    case Operation.plus:
+      print(a + b);
+      break;
+    case Operation.minus:
+      print(a - b);
+      break;
+    case Operation.multiply:
+      print(a * b);
+      break;
+    case Operation.divide:
+      print(a / b);
+      break;
+    default:
+      print("Operation No valid");
+  }
 }
