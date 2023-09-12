ae91d04f6f164bd9da537dec8c7f7124514591f6
bastndev <gohit24@gmail.com>
2023-09-12 09:30:47 -0500

    :peach: Update `new`

--- lib/section_4.dart
+++ lib/section_4.dart
@@ -173,8 +173,7 @@
 } */
 
 //--- TODO: 40. break and continue
-
-void main() {
+/* void main() {
   for (var i = 1; i <= 15; i++) {
     break;
     if (i % 3 == 0 && i % 5 == 0) {
@@ -184,6 +183,26 @@ void main() {
     } else if (i % 5 == 0) {
       print("buzz");
     } else {
+      print(i);
+    }
+  }
+  print("Done");
+} */
+void main() {
+  for (var i = 1; i <= 15; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      print("fizz buzz");
+      break;
+    }
+    if (i % 3 == 0) {
+      print("fizz");
+      continue;
+    }
+    if (i % 5 == 0) {
+      print("buzz");
+      continue;
+    }
+    {
       print(i);
     }
   }
