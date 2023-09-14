d52449ea7f1b602113a595903173df78208a08c8
bastndev <gohit24@gmail.com>
2023-09-14 10:45:46 -0500

    :coconut: Update `52`

--- lib/rock_paper_scissors.dart
+++ lib/rock_paper_scissors.dart
@@ -1,15 +1,47 @@
 import 'dart:io';
+import 'dart:math';
+
+enum Move { rock, paper, scissors }
 
 void main() {
   while (true) {
+    final rnd = Random();
     stdout.write('Rock, Paper or Scissors ? (r/p/s) ');
-
     final input = stdin.readLineSync();
-    if (input == 'r' || input == 'p' || input == 's'){
-      print("ehh");
-    }else if (input == 'q'){
+    if (input == 'r' || input == 'p' || input == 's') {
+
+      //Selecting player Move
+      var playerMove;
+
+      if (input == 'r') {
+        playerMove = Move.rock;
+      } else if (input == 'p') {
+        playerMove = Move.paper;
+      } else {
+        playerMove = Move.scissors;
+      }
+
+      //Select AI mode
+      final random = rnd.nextInt(3);
+      final aiMove = Move.values[random];
+
+      //Show player and AI
+      print('You Player: $playerMove');
+      print('AI Player: $aiMove');
+      if (playerMove == aiMove) {
+        print("It's a draw");
+
+        //Game logic
+      } else if (playerMove == Move.rock && aiMove == Move.scissors ||
+          playerMove == Move.paper && aiMove == Move.rock ||
+          playerMove == Move.scissors && aiMove == Move.paper) {
+        print("You Win");
+      } else {
+        print("you Lose");
+      }
+    } else if (input == 'q') {
       break;
-    }else{
+    } else {
       print("Invalid input");
     }
   }
