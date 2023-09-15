3f0e52c6ed94af9f232488c6acd6e632bd130d4f
bastndev <gohit24@gmail.com>
2023-09-15 11:06:20 -0500

    :arrow_up: UPdate `Loop`

--- lib/Paper_rock_scissors_practice.dart
+++ lib/Paper_rock_scissors_practice.dart
@@ -1,6 +1,12 @@
-// --- --- Practice Paper Rock Scissors TODO:
+// --- --- Practice Paper Rock Scissors -TODO:
 // import 'dart:io';
 
+import 'dart:io';
+
+enum Move {rock, paper, scissors }
 void main () {
-  
+  while(true){
+    stdout.write("Welcome this is game (r s p /q) ↧↧↧↧");
+    
+  }
 }
