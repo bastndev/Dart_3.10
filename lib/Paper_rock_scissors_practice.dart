3091113649dd20c7a8c1c328611f0e876b80f67a
bastndev <gohit24@gmail.com>
2023-09-15 11:29:22 -0500

    :arrow_up: Update `Random`

--- lib/Paper_rock_scissors_practice.dart
+++ lib/Paper_rock_scissors_practice.dart
@@ -1,12 +1,29 @@
 // --- --- Practice Paper Rock Scissors -TODO:
-// import 'dart:io';
-
 import 'dart:io';
+import 'dart:math';
 
 enum Move {rock, paper, scissors }
 void main () {
   while(true){
     stdout.write("Welcome this is game (r s p /q) ↧↧↧↧");
     final input = stdin.readLineSync();
+
+    //Selecting player move 
+    var playerMove;
+    if(input == 'r'){
+      playerMove = Move.rock;
+    }else if (input == 's'){
+      playerMove = Move.scissors;
+    }else if (input == 'p'){
+      playerMove = Move.paper;
+    }else if (input == 'q'){
+      break;
+    }else{
+      print("no Valid");
+    }
+
+    //--- Select AI Move
+    final randomNumber = Random().nextInt(3);
+    
   }
 }
