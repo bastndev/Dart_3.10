67f1f25d305e9c100760f9170b30416af1e28063
bastndev <gohit24@gmail.com>
2023-09-20 11:36:48 -0500

    :arro_up: Update `before`

--- lib/collections.dart
+++ lib/collections.dart
@@ -1,13 +1,22 @@
 //-TODO: . Lists
 /* void main (){
   var cities = ['Paris', 'Lima', 'Vietnam'];
-  print(cities[2]);
-}
- */
 
-void main () {
+  print(cities[2]);
+}*/
+
+/* void main () {
   var cities = ['Paris', 'Mexico', 'Lima' ];
   cities [1]= 'Juliaca';
 
   print(cities[1]);
-}
+} */
+//--- --- -FIXME: Before
+/* void main (){
+  var cities = ['Paris', 'Lima', 'Vietnam'];
+
+  for( var i = 0; i < cities.length; i++){
+    print(cities[i]);
+  } 
+} */
+// --- --- -FIXME: After
