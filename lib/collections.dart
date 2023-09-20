b7474ca58929e5d42592d4a729075fdff09fb092
bastndev <gohit24@gmail.com>
2023-09-20 11:11:10 -0500

    :arrow_up: Update `Cities`

--- lib/collections.dart
+++ lib/collections.dart
@@ -1,5 +1,13 @@
 //-TODO: . Lists
-void main (){
+/* void main (){
   var cities = ['Paris', 'Lima', 'Vietnam'];
   print(cities[2]);
+}
+ */
+
+void main () {
+  var cities = ['Paris', 'Mexico', 'Lima' ];
+  cities [1]= 'Juliaca';
+
+  print(cities[1]);
 }
