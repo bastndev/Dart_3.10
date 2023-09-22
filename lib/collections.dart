dc88e2359277aef2506393df99cca9349f5d0672
bastndev <gohit24@gmail.com>
2023-09-22 11:36:25 -0500

    :arrow_up: Update `new`

--- lib/collections.dart
+++ lib/collections.dart
@@ -222,7 +222,17 @@ void main() {
 
 //--- --- --- --- --- -- -TODO: 60. [Exercise] Sets
 
-void main (){
+void main() {
+  const a = {1, 3};
+  const b = {3, 5};
 
+  final c = a.union(b).difference(a.intersection(b));
+  print(c);
+
+  var sum = 0;
+  for (var value in c) {
+    sum += value;
+  }
+
+  print(sum);
 }
-
