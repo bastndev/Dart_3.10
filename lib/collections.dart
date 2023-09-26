a69d467ad3ebe4e02768ec763a322593f764f29b
bastndev <gohit24@gmail.com>
2023-09-26 10:34:56 -0500

    :arrow_up: Update `new`

--- lib/collections.dart
+++ lib/collections.dart
@@ -320,7 +320,13 @@ void main () {
     'age': 25,
     'height': 1.70,
   };
-  for ( var key in person.keys){
-    print(person [key]);
+  // for ( var key in person.keys){
+  //   print(person [key]);
+  // }
+  // for (var value in person.values){
+  //   print(value);
+  // }
+  for (var entry in person.entries){
+    print("${entry.key}: ${entry.value}");
   }
 }
