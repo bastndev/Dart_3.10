7fdfca7f606dd49e0ba4b799336c36122ae498b8
bastndev <gohit24@gmail.com>
2023-09-28 09:30:04 -0500

    :arrow_up: Update `new`

--- lib/collections.dart
+++ lib/collections.dart
@@ -348,7 +348,7 @@ void main() {
   }else {
     print("pineapple pizza is not on the menu");
   }
-} */ 
+} */
 
 /* void main (){
   const pizzaPrices = {
@@ -414,3 +414,34 @@ void main() {
     print(restaurant);
   }
 } */
+
+//--- -- -- -TODO: 67. [Exercise] Restaurant ratings
+void main() {
+  var restaurants = [
+    {
+      'name': 'Pizza Mario',
+      'cuisine': ' Italian',
+      'ratings': [4.0, 3.5, 4.5],
+    },
+    {
+      'name': 'Chez Anne',
+      'cuisine': ' French',
+      'ratings': [5.0, 4.5, 4.0],
+    },
+    {
+      'name': 'Navaratna',
+      'cuisine': ' India',
+      'ratings': [4.0, 4.5, 4.0],
+    }
+  ];
+  for (var restaurant in restaurants) {
+    final ratings = restaurant['ratings'] as List<double>;
+    var total = 0.0;
+    for (var rating in ratings){
+      total += rating;
+    }
+    final avgRating = total / ratings.length; 
+    restaurant['avgRating'] = avgRating;
+    print(restaurant);
+  }
+}
