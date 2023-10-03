151fcb4a6ebdabbb6e5dbb6632904475f3cc88d2
bastndev <gohit24@gmail.com>
2023-10-03 10:05:06 -0500

    :arrow_up: Update `new`

--- lib/collections.dart
+++ lib/collections.dart
@@ -553,3 +553,26 @@ void main() {
   ];
   print(colors);
 } */
+
+//TODO: 71. [Exercise] Shopping List
+//--FIXME: Normal mode
+void main (){
+  const bananas = 5;
+  const apples = 6; 
+  const grains ={
+    'pasta': '500g',
+    'rice': '1kg',
+  };
+  const addGrains = true;
+  var shoppingList = {};
+  if (bananas > 0){
+    shoppingList ['bananas'] = bananas;
+  }
+  if ( apples > 0){
+    shoppingList['apples'] = apples;
+  }
+  if (addGrains){
+    shoppingList.addAll(grains);
+  }
+  print(shoppingList);
+}
