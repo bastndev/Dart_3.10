c9ff6aa9581d375756c0858fece188f04d320de8
bastndev <gohit24@gmail.com>
2023-10-04 11:33:38 -0500

    :alien: Update `Exercis`e

--- data_base/totals.dart
+++ data_base/totals.dart
@@ -51,3 +51,32 @@ void main(List<String> arguments) {
 //printAll(durationByTag) */
 
 //--- --- --- -TODO: 77. Implementing the processing
+import 'dart:io';
+
+void main(List<String> arguments) {
+  if (arguments.isEmpty) {
+    print('Usage: dart totals.dart <inputFile.csv>');
+    exit(1);
+  }
+  final inputFile = arguments.first;
+  final lines = File(inputFile).readAsLinesSync();
+  final totalDurationByTag = <String, double>{};
+  lines.removeAt(0);
+  for (var line in lines) {
+    final values = line.split(',');
+    final durationStr = values[3].replaceAll('"', '');
+    final duration = double.parse(durationStr);
+    final tag = values[5].replaceAll('"', '');
+    final previousTotal = totalDurationByTag[tag];
+    if (previousTotal == null) {
+      totalDurationByTag[tag] = duration;
+    } else {
+      totalDurationByTag[tag] = previousTotal + duration;
+    }
+  }
+  for (var entry in totalDurationByTag.entries) {
+    final durationFormatted = entry.value.toStringAsFixed(1);
+    final tag = entry.key;
+    print('$tag: ${durationFormatted}h');
+  }
+}
