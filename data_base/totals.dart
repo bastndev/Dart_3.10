8791ef2a42a79c822ce1a6a98cee91cae0f133fc
bastndev <gohit24@gmail.com>
2023-10-04 10:56:34 -0500

    :arrow_up: Update `Arguments`

--- data_base/totals.dart
+++ data_base/totals.dart
@@ -1,4 +1,5 @@
-import 'dart:io';
+// --- --- --- -TODO: Arguments
+/* import 'dart:io';
 
 void main(List<String> arguments) {
   if (arguments.isEmpty) {
@@ -7,4 +8,19 @@ void main(List<String> arguments) {
   }
   final inputFile = arguments.first;
   print(inputFile);
-}
+} */
+
+// --- --- --- -TODO: 75. Reading files line by line
+/* import 'dart:io';
+
+void main(List<String> arguments) {
+  if (arguments.isEmpty) {
+    print('Usage: dart totals.dart <inputFile.csv>');
+    exit(1);
+  }
+  final inputFile = arguments.first;
+  final lines  = File (inputFile).readAsLinesSync();
+  for (var line  in lines ){
+    print(line);
+  }
+} */
