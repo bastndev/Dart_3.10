d16b76f877448aa9dbfcb9b1951fc7353b8aaa77
bastndev <gohit24@gmail.com>
2023-10-04 11:06:19 -0500

    :arrow_up: Update `new`

--- data_base/totals.dart
+++ data_base/totals.dart
@@ -24,3 +24,30 @@ void main(List<String> arguments) {
     print(line);
   }
 } */
+
+// --- --- --- -TODO: 76. Pseudocode for the processing logic
+/* import 'dart:io';
+
+void main(List<String> arguments) {
+  if (arguments.isEmpty) {
+    print('Usage: dart totals.dart <inputFile.csv>');
+    exit(1);
+  }
+  final inputFile = arguments.first;
+  final lines  = File (inputFile).readAsLinesSync();
+  for (var line  in lines ){
+    print(line);
+  }
+} 
+// lines - readFile (inputFile)
+// durationByTag = empty map
+// line.removeFirst()
+// for (line in lines)
+//   values - line.split(',')
+//   duration = values [3] 
+//   tag = values[5]
+//   update(durationByTag[tag], duration)
+//end
+//printAll(durationByTag) */
+
+//--- --- --- -TODO: 77. Implementing the processing
