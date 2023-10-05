f9b4b832b78b869827aeeced0cb249e4f32ebfb6
bastndev <gohit24@gmail.com>
2023-10-05 10:29:14 -0500

    :arrow_up: UPdate `new`

--- lib/null_safety.dart
+++ lib/null_safety.dart
@@ -1,8 +1,17 @@
-void main (){
+/* void main (){
   const person  = {'name': 'Gohit'};
   if(person['age'] == null){
     print('age is missing'); 
   }else {
     print(person['age']);
   }
+} */
+
+//-- -- -- -TODO: 79. Nullable and non-nullable variables
+/* void main() {
+  int a;
+  int b = 10;
+  
+  print(a + b ); //FIXME: whit error
 }
+ */
