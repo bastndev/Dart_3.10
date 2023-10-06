095439c7fa72ef6818d6e0f78c093d501634d33e
bastndev <gohit24@gmail.com>
2023-10-06 10:07:39 -0500

    :arrow_up: Update `new`

--- lib/9_funtions_basic.dart
+++ lib/9_funtions_basic.dart
@@ -1,9 +1,20 @@
 //--- -- --- --- -TODO: 87. Intro to functions
-void main (){
+/* void main (){
   sayHi();
   sayHi();
 }
 void sayHi(){
   print("Hello");
   print("Welcome");
-}
+} */
+
+//--- -- --- --- -TODO: 88. Function arguments
+/* void main() {
+  const name = 'Lucas';
+  const age = 25;
+  print("Hello my name is $name , y i'm $age years old");
+  const name2 = 'maria';
+  const age2 = 20;
+  print("Hello my name is $name2 , y i'm $age2 years old");
+}//-FIXME: Before 
+ */
