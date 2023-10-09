7bdbac221a5d233fc202df27ddfadba609fe9ebe
bastndev <gohit24@gmail.com>
2023-10-09 10:52:09 -0500

    :arrow_up: Update `new`

--- lib/9_funtions_basic.dart
+++ lib/9_funtions_basic.dart
@@ -313,3 +313,25 @@ String greet({required String name, required int age}) {
   }
   print("Total: \$$total");
 } */
+
+void main() {
+  const order = ['margherita', 'pepperoni', 'pineapple'];
+  final total = calculateTotal(order);
+  print("Total: \$$total");
+}
+
+double calculateTotal(List<String> order) {
+  const pizzaPrices = {
+    'margherita': 5.5,
+    'pepperoni': 7.5,
+    'vegetarian': 6.5,
+  };
+  var total = 0.0;
+  for (var item in order) {
+    final price = pizzaPrices[item];
+    if (price != null) {
+      total += price;
+    }
+  }
+  return total;
+}
