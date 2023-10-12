013f8ce1e39d7ee86e914b50659189bcc5e8ef52
bastndev <gohit24@gmail.com>
2023-10-11 20:37:43 -0500

    :arrow_up: UPdate `new`

--- lib/10_funtions_advance.dart
+++ lib/10_funtions_advance.dart
@@ -19,7 +19,9 @@ void welcome(String Function(String) greet, String name) {
 }
  */
 
-typedef Greet = String Function(String);
+/* typedef Greet = String Function(String); // -FIXME: Recommendation
+// typedef String Greet(String name);
+
 String sayHi(String name) => "Hi $name";
 String sayBonjour(String name) => "Bonjour $name";
 String sayHola(String name) => "Hola $name";
@@ -31,4 +33,4 @@ void main() {
 void welcome(Greet greet, String name) {
   print(greet(name));
   print('Welcome to this course');
-}
+} */
