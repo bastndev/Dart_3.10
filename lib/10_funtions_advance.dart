e7147e503f49a6d54a0c0fc32d4d45fdd04914c1
bastndev <gohit24@gmail.com>
2023-10-11 19:00:51 -0500

    :arrow_up: Upadte `new`

--- lib/10_funtions_advance.dart
+++ lib/10_funtions_advance.dart
@@ -1,8 +1,14 @@
-void greet(String name) {
+/* void greet(String name) {
   print('Hola, $name!');
 }
 
 void main() {
   greet('Juan');
   greet('Merry');
+}
+ */
+// --- --- --- -TODO: 100. Anonymous functions
+void main (){
+  final sayHi = (name) => 'Hi, $name';
+  print(sayHi('Gohit'));
 }
