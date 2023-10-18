6117cf9f28ac2cd37f60f29b7c4b26b917963ab5
bastndev <gohit24@gmail.com>
2023-10-18 12:27:57 -0500

    :arrow_up: Update `new`

--- lib/11_classes_basics.dart
+++ lib/11_classes_basics.dart
@@ -1,1 +1,13 @@
 //--- --- --TODO: 13. Section Intro
+class BankAccount {
+  double balance = 0;
+}
+
+void main() {
+  final bankAccount = BankAccount();
+  bankAccount.balance = 10;
+  print(bankAccount.balance);
+  final bankAccount2 = BankAccount();
+  bankAccount2.balance = 20;
+  print(bankAccount2.balance);
+}
