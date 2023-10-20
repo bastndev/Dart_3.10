79304dcf9f47a2dff2c3ddc8778487562cfa447e
bastndev <gohit24@gmail.com>
2023-10-19 22:21:27 -0500

    :arrow_up: Update `new2`

--- lib/11_classes_basics.dart
+++ lib/11_classes_basics.dart
@@ -1,13 +1,28 @@
 //--- --- --TODO: 13. Section Intro
-class BankAccount {
+/* class BankAccount {
   double balance = 0;
 }
 
 void main() {
   final bankAccount = BankAccount();
-  bankAccount.balance = 10;
+  bankAccount.balance = 100;
   print(bankAccount.balance);
   final bankAccount2 = BankAccount();
-  bankAccount2.balance = 20;
+  bankAccount2.balance = 200;
   print(bankAccount2.balance);
+} */
+
+//--- --- --TODO: 15. Instance methods
+class BankAccount{
+  int balance = 0;
+
+  void deposit(int amount){
+    balance += amount;
+  }
+}
+void main () {
+  final backAccount = BankAccount();
+  backAccount.deposit(100);
+  backAccount.deposit(10);
+  print(backAccount.balance);
 }
