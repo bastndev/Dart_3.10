3d4b7dbc885c08c70caf619dc485280ee68659e1
bastndev <gohit24@gmail.com>
2023-10-19 23:22:20 -0500

    :arrow_up: Update `new2`

--- lib/11_classes_basics.dart
+++ lib/11_classes_basics.dart
@@ -13,8 +13,7 @@ void main() {
 } */
 
 //--- --- --TODO: 15. Instance methodsclass BankAccount {
-
-class BankAccount {
+/* class BankAccount {
   int balance = 0;
   int balance2 = 0;
 
@@ -36,3 +35,58 @@ void main() {
   print("Balance1: ${bankAccount.balance}");
   print("Balance2: ${bankAccount.balance2}");
 }
+ */
+
+class BankAccount {
+  double balance = 0;
+
+  void deposit(double amount) {
+    balance += amount;
+  }
+
+  bool withdraw(double amount) {
+    if (balance > amount) {
+      balance -= amount;
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+void main() {
+  final bankAccount = BankAccount();
+  bankAccount.deposit(100);
+  bankAccount.deposit(50);
+  final success1 = bankAccount.withdraw(100);
+  print("success: $success1, balance: ${bankAccount.balance}");
+  final success2 = bankAccount.withdraw(100);
+  print("success: $success2, balance: ${bankAccount.balance}");
+}
+
+/* class BankAccount {
+  double balance = 0;
+
+  void deposit(double amount) {
+    balance += amount;
+  }
+
+  bool withdraw(double amount) {
+    if (balance > amount) {
+      balance -= amount;
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+void main() {
+  final bankAccount = BankAccount();
+  bankAccount.deposit(100);
+  bankAccount.deposit(50);
+  final success1 = bankAccount.withdraw(100);
+  print("success: $success1, balance: ${bankAccount.balance}");
+  final success2 = bankAccount.withdraw(100);
+  print("success: $success2, balance: ${bankAccount.balance}");
+}
+ */
