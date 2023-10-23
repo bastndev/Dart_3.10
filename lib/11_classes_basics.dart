f29766e9efe29994d62de909b2d675296d7b5d70
bastndev <gohit24@gmail.com>
2023-10-23 11:39:15 -0500

    :arrow_up: Update `new`

--- lib/11_classes_basics.dart
+++ lib/11_classes_basics.dart
@@ -90,3 +90,27 @@ void main() {
   print("success: $success2, balance: ${bankAccount.balance}");
 }
  */
+class BankAccount {
+  BankAccount(double balance) {
+    this.balance = balance;
+  }
+  double balance = 0;
+
+  void deposit(double amount) {
+    balance += amount;
+  }
+
+  bool withdraw(double amount) {
+    if (balance > amount) {
+      balance -= amount;
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+void main() {
+  final bankAccount = BankAccount(100);
+  print(bankAccount.balance);
+}
