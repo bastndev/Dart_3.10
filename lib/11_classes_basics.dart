e3ac67d583ebf5b796386b9f9255b016ff20bbd4
bastndev <gohit24@gmail.com>
2023-10-24 12:19:59 -0500

    :arrow_up: Update `new`

--- lib/11_classes_basics.dart
+++ lib/11_classes_basics.dart
@@ -115,6 +115,32 @@ void main() {
   print(bankAccount.balance);
 } */
 
+/* class BankAccount {
+  BankAccount({
+    required this.accountHolder,
+    this.balance = 0,
+  });
+  String accountHolder;
+  double balance;
+
+  void deposit(double amount) {
+    balance += amount;
+  }
+
+  bool withdraw(double amount) {
+    if (balance > amount) {
+      balance -= amount;
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+void main() {
+  final bankAccount = BankAccount(accountHolder: 'Gohit', balance: 100);
+  print(bankAccount.balance.toInt());
+} */
 class BankAccount {
   BankAccount({
     required this.accountHolder,
