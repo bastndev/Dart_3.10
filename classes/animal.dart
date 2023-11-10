f87fe509dbae6c5b095b3ea1a39da2bf3010768c
bastndev <gohit24@gmail.com>
2023-11-10 10:42:15 -0500

    :arrow_up: Update `New`

--- classes/animal.dart
+++ classes/animal.dart
@@ -1,8 +1,20 @@
 class Animal {
-  void sleep() => print('Sleep');
+  String sleep() {
+    print('Sleep');
+    return 'is sleeping';
+  }
+}
+
+class Dog extends Animal {
+  void bark() => print('Bark');
 }
 
 void main() {
   final animal = Animal();
   animal.sleep();
+
+  final dog = Dog();
+  dog.bark();
+
+  print("My dog ${dog.sleep()}");
 }
