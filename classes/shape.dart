095b2a70bdeb093df74f004a49f208914f80ce0e
bastndev <gohit24@gmail.com>
2023-11-11 10:45:03 -0500

    :arrow_up: Update `new`

--- classes/shape.dart
+++ classes/shape.dart
@@ -2,6 +2,7 @@
 abstract class Shape {
   double get area;
 }
+
 class Square extends Shape {
   Square(this.side);
   final double side;
@@ -10,7 +11,26 @@ class Square extends Shape {
   double get area => side * side;
 }
 
+class Circle extends Shape {
+  Circle(this.radius);
+  final double radius;
+
+  @override
+  double get area => radius * radius;
+}
+
+void printArea(Shape shape) {
+  print(shape.area);
+}
+
 void main() {
   final square = Square(10);
-  print(square.area);
+  printArea(square);
+  final Shape circle = Circle(5);
+  printArea(circle);
+  final shapes = [
+    Square(2),
+    Circle(3),
+  ];
+  shapes.forEach(printArea);
 }
