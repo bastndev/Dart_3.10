4862090273caf664d216116de1056208b41c0437
bastndev <gohit24@gmail.com>
2023-11-11 10:29:14 -0500

    :arrow_up: Update `new`

--- classes/shape.dart
+++ classes/shape.dart
@@ -1,1 +1,16 @@
 //--- --- --- -TODO: 134. Abstract classes
+abstract class Shape {
+  double get area;
+}
+class Square extends Shape {
+  Square(this.side);
+  final double side;
+
+  @override
+  double get area => side * side;
+}
+
+void main() {
+  final square = Square(10);
+  print(square.area);
+}
