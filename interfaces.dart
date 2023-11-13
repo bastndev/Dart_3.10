7f5a97a42c9ccf078c820e6806c2a61c068589d6
bastndev <gohit24@gmail.com>
2023-11-13 10:49:39 -0500

    :arrow_up: Update `new`

---
+++ interfaces.dart
@@ -0,0 +1,34 @@
+abstract class InterfaceA {
+  void a();
+}
+
+abstract class InterfaceB {
+  void b();
+}
+
+class AB implements InterfaceA, InterfaceB {
+  @override
+  void a() {
+    // TODO: implement a
+  }
+
+  @override
+  void b() {
+    // TODO: implement b
+  }
+}
+
+abstract class Base {
+  void foo();
+  void bar() => print('bar');
+}
+
+class Subclass implements Base {
+  @override
+  void foo() => print('foo');
+  
+  @override
+  void bar() {
+    // TODO: implement bar
+  }
+}
