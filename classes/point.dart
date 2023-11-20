d89cd4953a547333a0bd58da2816930ed1a017ce
bastndev <gohit24@gmail.com>
2023-11-20 11:02:32 -0500

    :arrow_up: Update `new`

--- classes/point.dart
+++ classes/point.dart
@@ -13,7 +13,7 @@ void main() {
   print(5 == 5);
 }
  */
-class Point {
+/* class Point {
   const Point(this.x, this.y);
 
   final int x;
@@ -25,4 +25,25 @@ class Point {
 
 void main() {
   print(Point(0, 0) == Point(0, 0));
+} */
+class Point {
+  const Point(this.x, this.y);
+
+  final int x;
+  final int y;
+
+  @override
+  String toString() => 'Point($x, $y)';
+
+  @override
+  bool operator ==(Object other){
+    if (other is Point) {
+      return this.x == other.x &&  y == other.y;
+    }
+    return false;
+  }
+}
+
+void main() {
+  print(Point(3, 0) == Point(3, 0));
 }
