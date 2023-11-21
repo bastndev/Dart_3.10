a173ce034ec86fe5ba5999dc770d64e5070aaaec
bastndev <gohit24@gmail.com>
2023-11-21 18:23:03 -0500

    :arrow_up: Update `new`

--- classes/widget.dart
+++ classes/widget.dart
@@ -1,6 +1,57 @@
-abstract class Widget {}
+/* abstract class Widget {}
 
 class Text extends Widget {
   Text(this.text);
   final String text;
+}
+
+class Button extends Widget {
+  Button({required this.child, this.onPressed});
+  final Widget child;
+  final void Function()? onPressed;
+}
+
+void main() {
+  final button = Button(
+    child: Text('Hello world'),
+    onPressed: (() => print('Button pressed')),
+  );
+}
+ */
+abstract class Widget {
+  void build(); // Agregamos un método abstracto
+}
+
+class Text extends Widget {
+  Text(this.text);
+  final String text;
+
+  @override
+  void build() {
+    // Implementación del método abstracto
+    print('Building Text widget with text: $text');
+  }
+}
+
+class Button extends Widget {
+  Button({required this.child, this.onPressed});
+  final Widget child;
+  final void Function()? onPressed;
+
+  @override
+  void build() {
+    // Implementación del método abstracto
+    print('Building Button widget');
+  }
+}
+
+void main() {
+  final button = Button(
+    child: Text('Hello world'),
+    onPressed: (() => print('Button pressed')),
+  );
+
+  // Llamamos al método build en lugar de imprimir directamente
+  button.build();
+  button.child.build(); // También llamamos al método build en el widget hijo
 }
