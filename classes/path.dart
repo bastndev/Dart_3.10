3063e137c5383cd0c034c27e3b96c3cf446dcc13
bastndev <gohit24@gmail.com>
2023-11-22 17:20:00 -0500

    :arrow_up: Update `new`

--- classes/path.dart
+++ classes/path.dart
@@ -1,4 +1,4 @@
-import 'dart:math';
+/* import 'dart:math';
 
 class ClosedPath2 {
   const ClosedPath2(this.points);
@@ -24,4 +24,47 @@ void main() {
     ..lineTo(Point(2, 2))
     ..lineTo(Point(0, 2))
     ..lineTo(Point(0, 0));
+}
+ */
+
+//FIXME: AI
+import 'dart:math';
+
+class ClosedPath2 {
+  const ClosedPath2(this.points);
+  final List<Point> points;
+}
+
+class ClosedPath {
+  List<Point> _points = [];
+
+  void moveTo(Point point) {
+    _points = [point];
+  }
+
+  void lineTo(Point point) {
+    _points.add(point);
+  }
+
+  ClosedPath2 close() {
+    if (_points.isNotEmpty) {
+      _points.add(_points.first);
+      return ClosedPath2(List<Point>.unmodifiable(_points));
+    } else {
+      throw StateError('Path is empty. Call moveTo() first.');
+    }
+  }
+
+  List<Point> get points => List<Point>.unmodifiable(_points);
+}
+
+void main() {
+  final path = ClosedPath()
+    ..moveTo(Point(0, 0))
+    ..lineTo(Point(2, 0))
+    ..lineTo(Point(2, 2))
+    ..lineTo(Point(0, 2))
+    ..close();
+
+  print(path.points);
 }
