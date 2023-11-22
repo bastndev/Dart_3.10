906556e847618fe00b94ab40ea8cb2bc5b8e0139
bastndev <gohit24@gmail.com>
2023-11-22 17:13:46 -0500

    :arrow_up: Update `new`

--- classes/path.dart
+++ classes/path.dart
@@ -3,12 +3,21 @@ import 'dart:math';
 class ClosedPath {
   List<Point> _points = [];
 
-  void moveTo(Point point){
+  void moveTo(Point point) {
     _points = [point];
   }
 
-  void lineTo(Point point){
+  void lineTo(Point point) {
     _points.add(point);
   }
 }
 
+void main() {
+  final path = ClosedPath();
+
+  path.moveTo(Point(0, 0));
+  path.lineTo(Point(2, 0));
+  path.lineTo(Point(2, 2));
+  path.lineTo(Point(0, 2));
+  path.lineTo(Point(0, 0));
+}
