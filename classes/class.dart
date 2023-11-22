a3e46f40bd3188a3fa4274fc2b586695fa1c393a
bastndev <gohit24@gmail.com>
2023-11-22 18:26:19 -0500

    :arrow_up: Update `new`

---
+++ classes/class.dart
@@ -0,0 +1,14 @@
+class Animal {
+  late String name;
+  late String type;
+
+  Animal({required String name,  String type = ''}) {
+    this.name = name;
+    this.type = type;
+  }
+}
+
+void main() {
+  Animal animal = Animal(name: 'Lion', type: 'carnivorous');
+  print('Animal: ${animal.name} - ${animal.type}');
+}
