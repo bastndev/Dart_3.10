ccfb0c2abb42528313e3dcab6081a16c580150c0
bastndev <gohit24@gmail.com>
2023-11-22 14:15:55 -0500

    :arrow_up: Update `new`

---
+++ classes/shapes.dart
@@ -0,0 +1,36 @@
+import 'dart:math';
+
+abstract class Shape {
+  double get area;
+  double get perimeter;
+
+  void printValues() {
+    print('area: $area, perimeter: $perimeter');
+  }
+}
+
+class Square extends Shape {
+  Square(this.side);
+  final double side;
+
+  @override
+  double get area => side * side;
+
+  @override
+  double get perimeter => 4 * side;
+}
+
+class Circle extends Shape {
+  Circle(this.radius);
+  final double radius;
+
+  @override
+  double get area => pi * radius * radius;
+
+  @override
+  double get perimeter => 2 * pi * radius;
+}
+
+void main() {
+
+}
