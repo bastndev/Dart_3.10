b4a857e7a9680d4fce6efba2ee4ff5fec1ebac50
bastndev <gohit24@gmail.com>
2023-11-22 16:49:02 -0500

    :arrow_up: Update `new`

--- classes/credentials.dart
+++ classes/credentials.dart
@@ -13,4 +13,15 @@ class Credentials {
       password: password ?? this.password,
     );
   }
+
+  @override
+  String toString() => 'Credentials($email,$password)';
+}
+
+void main() {
+  const credentials = Credentials();
+  final updated1 = credentials.copyWith(email: 'me@exaple.com');
+  print(updated1);
+  final updated2 = credentials.copyWith(password: 'to-easy');
+  print(updated2);
 }
