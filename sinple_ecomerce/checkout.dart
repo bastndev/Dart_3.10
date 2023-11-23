2f651c59f36d14ac4c28cc7e9941d1e93f4de774
bastndev <gohit24@gmail.com>
2023-11-23 14:33:05 -0500

    :arrow_up: Update `new`

---
+++ sinple_ecomerce/checkout.dart
@@ -0,0 +1,14 @@
+class Product {
+  Product({required this.id, required this.name, required this.price});
+
+  final double price;
+  final String name;
+  final int id;
+}
+
+class Item {
+  Item({required this.product, required this.quantity});
+
+  final Product product;
+  final int quantity;
+}
