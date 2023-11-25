3794f7fe64185bd4854eecea0e2931f50f6e997a
bastndev <gohit24@gmail.com>
2023-11-25 08:52:17 -0500

    :arrow_up: Update `new`

--- MIXINS_EXTENSIONS/interable_sum.dart
+++ MIXINS_EXTENSIONS/interable_sum.dart
@@ -5,8 +5,16 @@
 void main() {
   final sum = [1, 2, 3].sum();
 } */
-extension InterableX on Iterable<double> {
+/* extension InterableX on Iterable<double> {
   double sum() => reduce((value, element) => value + element);
+}
+
+void main() {
+  final sum = [1.0, 2.0, 3.0].sum();
+  print(sum);
+} */
+extension InterableX on Iterable<num> {
+  num sum() => reduce((value, element) => value + element);
 }
 
 void main() {
