4e9723786be84e057684ee3cbe75ce61ee52aa48
bastndev <gohit24@gmail.com>
2023-11-24 21:37:40 -0500

    :arrow_up: Update `new`

--- MIXINS_EXTENSIONS/extensions.dart
+++ MIXINS_EXTENSIONS/extensions.dart
@@ -1,8 +1,6 @@
-extension on String {
-  int? toIntOrNull() => int.tryParse(this);
-}
+import 'number_parding.dart';
 
-void main () {
+void main() {
   int.tryParse('124');
   '124'.toIntOrNull();
   '456'.toIntOrNull();
