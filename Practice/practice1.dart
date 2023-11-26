f05799023a8dc0b4de48c3a54d4efff0770c22d6
bastndev <gohit24@gmail.com>
2023-11-26 15:24:31 -0500

    :arrow_up: Update `new`

--- Practice/practice1.dart
+++ Practice/practice1.dart
@@ -1,4 +1,5 @@
-import 'dart:io';
+// --- --- -TODO: Practice numbers int, double and int.
+/* import 'dart:io';
 
 int sum(int x, int y) => x + y;
 
@@ -11,4 +12,19 @@ void main() {
 
   int result = sum(num1, num2);
   print('La suma de $num1 y $num2 es: $result');
+} */
+
+import 'dart:io';
+
+num sum(num x, num y) => x + y;
+
+void main() {
+  print('Enter first Number:');
+  num num1 = num.parse(stdin.readLineSync()!);
+
+  print('Enter second Number');
+  num num2 = num.parse(stdin.readLineSync()!);
+
+  num result = sum(num1, num2);
+  print('The sum of $num1 and $num2 is:$result');
 }
