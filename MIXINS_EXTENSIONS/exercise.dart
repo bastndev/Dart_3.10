54d01e1669c94aa1341cb41ba68469f5b64538b0
bastndev <gohit24@gmail.com>
2023-11-26 11:39:17 -0500

    :arrow_up: Update `new`

--- MIXINS_EXTENSIONS/exercise.dart
+++ MIXINS_EXTENSIONS/exercise.dart
@@ -37,3 +37,22 @@ void main() {
   }
   // output [1, 2, 3, 4 , 5]
 } */
+
+extension Number on int {
+  List<int> numberTo(int other) {
+    if (other < this) {
+      return [];
+    }
+    var list = [this];
+    for (var e = this + 1; e <= other; e++) {
+      list.add(e);
+    }
+    return list;
+  }
+}
+
+void main() {
+  for (var e in 1.numberTo(6)) {
+    print(e);
+  }
+}
