31961129f6b262cf1012eaf8715676e1afd238fb
bastndev <gohit24@gmail.com>
2023-11-27 11:57:54 -0500

    :arrow_up: Update `new`

--- ERROR_EXCEPTIONS/exceptions.dart
+++ ERROR_EXCEPTIONS/exceptions.dart
@@ -10,7 +10,7 @@ void main() {
   final f = Fraction(3, 5);
   print(f.value);
 } */
-//--- -FIXME: EXCEPTIONS 
+//--- -FIXME: EXCEPTIONS
 /* class Fraction {
   Fraction(this.numeration, this.denominator);
   final int numeration;
@@ -38,7 +38,7 @@ void main() {
   print(f.value);
 } */
 
-//--- -FIXME: you can use all kinds of operations. 
+//--- -FIXME: you can use all kinds of operations.
 /* class Fraction {
   Fraction(this.numeration, this.denominator);
   final num numeration;
@@ -52,3 +52,34 @@ void main() {
   print(f.value);
 } */
 
+// ignore_for_file: deprecated_member_use
+class Fraction {
+  Fraction(this.numeration, this.denominator) {
+    if (denominator == 0) {
+      throw IntegerDivisionByZeroException();
+    }
+  }
+  final int numeration;
+  final int denominator;
+
+  double get value => numeration / denominator;
+}
+
+void testFraction() {
+  try {
+    final f = Fraction(3, 5);
+    print(f.value);
+  } on IntegerDivisionByZeroException catch (e) {
+    print(e);
+    rethrow;
+  } on Exception catch (e) {
+    print(e);
+  } finally {
+    print('TestFraction dome');
+  }
+}
+
+void main() {
+  testFraction();
+  print('done');
+}
