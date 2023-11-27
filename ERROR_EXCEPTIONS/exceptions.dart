38edff92d643b3451684f8243d845e5d39ea095b
bastndev <gohit24@gmail.com>
2023-11-27 11:31:55 -0500

    :arrow_up: Update `new`

--- ERROR_EXCEPTIONS/exceptions.dart
+++ ERROR_EXCEPTIONS/exceptions.dart
@@ -10,6 +10,19 @@ void main() {
   final f = Fraction(3, 5);
   print(f.value);
 } */
+//--- -FIXME: EXCEPTIONS 
+/* class Fraction {
+  Fraction(this.numeration, this.denominator);
+  final int numeration;
+  final int denominator;
+
+  double get value => numeration / denominator;
+}
+
+void main() {
+  final f = Fraction(3, 0);
+  print(f.value);
+} */
 
 //--- -Sum Practice
 /* class Fraction {
@@ -38,3 +51,4 @@ void main() {
   final f = Fraction(3, 5);
   print(f.value);
 } */
+
