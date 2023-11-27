9e8d12ac5e0a855d60b227d5bff9e5aab7cb77de
bastndev <gohit24@gmail.com>
2023-11-27 10:57:07 -0500

    :arrow_up: Update `new`

--- ERROR_EXCEPTIONS/assert.dart
+++ ERROR_EXCEPTIONS/assert.dart
@@ -21,3 +21,18 @@ void main() {
   print(invalidAge);
 } */
 
+//--- -TODO: Unhandled exception:'file:///D:/!Projects/Dart_3.10/ERROR_EXCEPTIONS/assert.dart': Failed assertion: line 31 pos 10: 'email.isNotEmpty': is not tru
+class PositiveInt {
+  const PositiveInt(this.value)
+      : assert(value >= 0, 'Value cannot be negative');
+  final int value;
+}
+
+void singIng(String email, String password) {
+  assert(email.isNotEmpty);
+  assert(password.isNotEmpty);
+}
+
+void main() {
+  singIng('', '');
+}
