aae152c16a804183b81417962aa360ed9fef5b67
bastndev <gohit24@gmail.com>
2023-11-27 11:27:18 -0500

    :arrow_up: Update `new`

--- ERROR_EXCEPTIONS/exceptions.dart
+++ ERROR_EXCEPTIONS/exceptions.dart
@@ -1,9 +1,22 @@
-class Fraction {
+/* class Fraction {
   Fraction(this.numeration, this.denominator);
   final int numeration;
   final int denominator;
 
   double get value => numeration / denominator;
+}
+
+void main() {
+  final f = Fraction(3, 5);
+  print(f.value);
+} */
+
+class Fraction {
+  Fraction(this.numeration, this.denominator);
+  final int numeration;
+  final int denominator;
+
+  int get value => numeration + denominator;
 }
 
 void main() {
