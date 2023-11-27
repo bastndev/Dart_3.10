fcc79bd226a13730c4dbbdb12cc5a35a4458292c
bastndev <gohit24@gmail.com>
2023-11-27 11:29:50 -0500

    :arrow_up: Update `new`

--- ERROR_EXCEPTIONS/exceptions.dart
+++ ERROR_EXCEPTIONS/exceptions.dart
@@ -24,3 +24,17 @@ void main() {
   final f = Fraction(3, 5);
   print(f.value);
 } */
+
+//--- -FIXME: you can use all kinds of operations. 
+/* class Fraction {
+  Fraction(this.numeration, this.denominator);
+  final num numeration;
+  final num denominator;
+
+  num get value => numeration * denominator;
+}
+
+void main() {
+  final f = Fraction(3, 5);
+  print(f.value);
+} */
