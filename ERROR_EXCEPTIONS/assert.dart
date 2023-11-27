d572b8b2daf8be15045faa1f50cc082b65b07d40
bastndev <gohit24@gmail.com>
2023-11-27 10:47:08 -0500

    :arrow_up: Update `new`

---
+++ ERROR_EXCEPTIONS/assert.dart
@@ -0,0 +1,11 @@
+//--- -TODO: Unhandled exception:'file:///D:/!Projects/Dart_3.10/ERROR_EXCEPTIONS/assert.dart': Failed assertion: line 2 pos 42: 'value >= 0': Value cannot be negative
+/* class PositiveInt {
+  const PositiveInt(this.value) : assert(value >= 0, 'Value cannot be negative');
+  final int value;
+}
+
+void main() {
+  final invalidAge = PositiveInt(-1);
+  print(invalidAge);
+}
+ */
