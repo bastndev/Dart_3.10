4658d7a18d4d603cbae8da6467feff00db891f97
bastndev <gohit24@gmail.com>
2023-11-29 11:39:40 -0500

    :arrow_up: Update `new`

--- ASYNC_PROGRAMMING/futures.dart
+++ ASYNC_PROGRAMMING/futures.dart
@@ -49,7 +49,7 @@ void main() {
       .whenComplete(() => print('Done'));
 } */
 
-Future<String> fetchUserOrder() => Future.delayed(
+/* Future<String> fetchUserOrder() => Future.delayed(
       Duration(seconds: 2),
       () => ('Coffee'),
     );
@@ -60,4 +60,15 @@ void main() {
       .then((order) => print('Order is ready: $order'))
       .catchError((error) => print(error))
       .whenComplete(() => fetchUserOrder().then((value) => null));
+} */
+
+Future<String> fetchUserOrder() => Future.delayed(
+      Duration(seconds: 2),
+      () => ('Coffee'),
+    );
+
+void main() async { // --- espera que siga el prgrama y luego se cargan en segundo plano, no lo bloquea
+  print('Program Started');
+  final order = await fetchUserOrder();
+  print(order);
 }
