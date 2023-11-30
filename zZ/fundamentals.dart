1424a2506a25d5d69d65a2c82edb4f03a9df5d0c
bastndev <gohit24@gmail.com>
2023-11-30 09:43:16 -0500

    :arrow_up: Update `new`

--- zZ/fundamentals.dart
+++ zZ/fundamentals.dart
@@ -36,3 +36,18 @@ void main() {
   int result = ((sum.a + sum.b) * 4) * 5 + 3;
   print(result);
 } */
+
+class Operation {
+  Operation(this.a, this.b);
+
+  final int a;
+  final int b;
+
+  int value() => a + b;
+}
+
+void main() {
+  final sum = Operation(1, 1);
+  int result = ((sum.value()) * 4) * 5 + 3;
+  print(result);
+}
