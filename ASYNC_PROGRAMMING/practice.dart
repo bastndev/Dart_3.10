c49ab3499a162cb7dd62e4801c05fc5c85730a54
bastndev <gohit24@gmail.com>
2023-11-30 18:27:52 -0500

    :arrow_up: Update `new`

---
+++ ASYNC_PROGRAMMING/practice.dart
@@ -0,0 +1,20 @@
+class MayerHumor {
+  final Duration _positive = Duration(milliseconds: 500);
+  final int _limit = 5;
+
+  Future<void> startCounter() async {
+    await for (int value in Stream.periodic(this._positive, (i) => i)
+        .takeWhile((value) => value < this._limit)) {
+      print('$value');
+    }
+    if (this._limit >= 5) {
+      print('Good Humor');
+    } else {
+      print('Bad Humor');
+    }
+  }
+}
+
+void main() async {
+  await MayerHumor().startCounter();
+}
