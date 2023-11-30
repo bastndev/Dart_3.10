a492f7b6e8e88b730f90c4a99164a5adb96ee0b3
bastndev <gohit24@gmail.com>
2023-11-30 16:16:36 -0500

    :arrow_up: Update `new`

--- ASYNC_PROGRAMMING/strams.dart
+++ ASYNC_PROGRAMMING/strams.dart
@@ -277,3 +277,16 @@ Future<void> main() async {
  */
 
 //--- --- -FIXME: 172. [Exercise] Fizz-buzz with streams
+void fizzBuzz() {
+  for (var i = 1; i <= 15; i++) {
+    if (i % 3 == 0 && i % 5 == 0) {
+      print('fizz-buzz');
+    } else if (i % 3 == 0) {
+      print('fizz');
+    } else if (i % 5 == 0) {
+      print('buzz');
+    } else {
+      print(i);
+    }
+  }
+}
