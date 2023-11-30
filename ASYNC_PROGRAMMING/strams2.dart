fe0ea2659382ac5838a96745058cf3fc3d6d6931
bastndev <gohit24@gmail.com>
2023-11-30 17:08:08 -0500

    :arrow_up: Update `new`

--- ASYNC_PROGRAMMING/strams2.dart
+++ ASYNC_PROGRAMMING/strams2.dart
@@ -1,6 +1,24 @@
-void main () {
+/* void main() {
   Stream.fromIterable([1, 2, 3, 4]);
   Stream.value(10);
   Stream.error(Exception('Error sorry!'));
   Stream.empty();
+  Stream.fromFuture(Future.delayed(Duration(seconds: 1), () => 42));
+  Stream.periodic(Duration(seconds: 1), (index) => index);
+}
+ */
+void main() {
+  // Stream.fromFuture
+  Stream.fromFuture(Future.delayed(Duration(seconds: 5), () => 42)).listen(
+    (int value) {
+      print('Stream.fromFuture: $value');
+    },
+  );
+
+  // Stream.periodic
+  Stream.periodic(Duration(seconds: 1), (index) => index).listen(
+    (int value) {
+      print('Stream.periodic: $value');
+    },
+  );
 }
