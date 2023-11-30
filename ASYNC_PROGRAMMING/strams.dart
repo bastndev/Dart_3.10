495fa2bbfd4bb272206010350f227d66687bef61
bastndev <gohit24@gmail.com>
2023-11-30 16:00:02 -0500

    :arrow_up: Update `new`

--- ASYNC_PROGRAMMING/strams.dart
+++ ASYNC_PROGRAMMING/strams.dart
@@ -242,20 +242,36 @@ void main() async{
   print(sum);
 } */
 
-//  --- -FIXME: Two methods 
-Future<int> sumStream(Stream<int> stream) async {
-  var sum = 0;
-  await for (var value in stream) {
-    sum += value;
-  }
-  return sum;
-}
+//  --- -FIXME: Two methods
+// Future<int> sumStream(Stream<int> stream) async {
+//   var sum = 0;
+//   await for (var value in stream) {
+//     sum += value;
+//   }
+//   return sum;
+// }
 
-Future<int> sumStream2(Stream<int> stream) =>
+/* Future<int> sumStream2(Stream<int> stream) =>
     stream.reduce((previous, element) => previous + element);
 
+Stream<int> countStream(int n) async* {
+  for (var i = 1; i <= n; i++) {
+    // await Future.delayed(Duration(seconds: 1));
+    // print(i);
+    yield i;
+  }
+}
+
+Iterable<int> count(int n) sync* {
+  for (var i = 1; i <= n; i++) {
+    yield i;
+  }
+}
+
 Future<void> main() async {
-  final stream = Stream<int>.fromIterable([10, 2, 3, 4]);
-  final sum = await sumStream2(stream);
+  final stream = Stream<int>.fromIterable([1, 2, 3, 4]);
+  final stream2 = countStream(4);
+  final sum = await sumStream2(stream2);
   print("The sum is:$sum");
 }
+ */
