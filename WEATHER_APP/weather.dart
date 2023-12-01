79febd83183fcc7fa831b4ef20b2487d534fc92c
bastndev <gohit24@gmail.com>
2023-12-01 13:52:10 -0500

    :arrow_up: Update `new`

--- WEATHER_APP/weather.dart
+++ WEATHER_APP/weather.dart
@@ -5,9 +5,16 @@ class Weather {
     required this.maxTemp,
     required this.temp,
   });
-  
+
   final String weatherState;
   final double minTemp;
   final double maxTemp;
   final double temp;
+
+  factory Weather.fromJson(Map<String, Object?> json) => Weather(
+        weatherState: json[''] as String,
+        minTemp: json[''] as double,
+        maxTemp: json[''] as double,
+        temp: json[''] as double,
+      );
 }
