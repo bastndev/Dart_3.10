8f0a0ecdcda095600475238dadbc45b1f675b36f
bastndev <gohit24@gmail.com>
2023-12-01 09:52:55 -0500

    :arrow_up: Update `new`

--- WEATHER_APP/main.dart
+++ WEATHER_APP/main.dart
@@ -1,3 +1,8 @@
 void main(List<String> arguments) {
-  
+  if (arguments.length != 1) {
+    print('Syntax: dart bin/main.dart <city>');
+    return;
+  }
+  final city = arguments.first;
+  print(city);
 }
