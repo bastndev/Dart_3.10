ca4529b8ef003070fc9444bccd0b3e03eadf6e23
bastndev <gohit24@gmail.com>
2023-12-01 10:59:13 -0500

    :arrow_up: Update `new`

--- WEATHER_APP/weather_api.dart
+++ WEATHER_APP/weather_api.dart
@@ -1,7 +1,19 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 class WeatherAppClient {
   static const basUrl = 'https://www.metaweather.com/api';
 
   Future<int> getLocationId(String city) async {
     final locationUrl = Uri.parse('$basUrl/location/search/?query=$city');
+    final locationResponse = await http.get(locationUrl);
+
+    if (locationResponse.statusCode != 200) {
+      throw Exception('Error getting locationID for city: $city');
+    }
+    final locationJson = jsonDecode(locationResponse.body);
+    print(locationJson);
+    return 0;
   }
 }
