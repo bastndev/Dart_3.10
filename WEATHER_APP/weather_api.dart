f165c24a95bdfcc5c39ffa14a4f525c1047884f2
bastndev <gohit24@gmail.com>
2023-12-01 11:30:04 -0500

    :arrow_up: Update `new`

--- WEATHER_APP/weather_api.dart
+++ WEATHER_APP/weather_api.dart
@@ -1,18 +1,29 @@
 import 'dart:convert';
-
 import 'package:http/http.dart' as http;
 
 class WeatherAppClient {
-  static const basUrl = 'https://www.metaweather.com/api';
-
+  static const baseUrl = 'https://api.openweathermap.org/data/2.5';
+  static const apiKey = 'ee7e131171555e2664223e127d11702f';
   Future<int> getLocationId(String city) async {
-    final locationUrl = Uri.parse('$basUrl/location/search/?query=$city');
-    final locationResponse = await http.get(locationUrl);
+    try {
+      final locationUrl = Uri.parse('$baseUrl/weather?q=$city&appid=$apiKey');
+      final locationResponse = await http.get(locationUrl);
 
-    if (locationResponse.statusCode != 200) {
-      throw Exception('Error getting locationID for city: $city');
+      if (locationResponse.statusCode == 200) {
+        final Map<String, dynamic> locationJson =
+            jsonDecode(locationResponse.body);
+
+        if (locationJson.containsKey('id')) {
+          return locationJson['id'] as int;
+        } else {
+          throw Exception('Location ID not found for city: $city');
+        }
+      } else {
+        throw Exception(
+            'Error getting locationID for city: $city. Status code: ${locationResponse.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Error getting locationID for city: $city. Details: $e');
     }
-    final locationJson = jsonDecode(locationResponse.body) as List;
-    return locationJson.first['woeid'] as int;
   }
 }
